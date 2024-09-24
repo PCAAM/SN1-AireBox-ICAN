1b8654299c0b71e36fa36f3b838387881cb65226
root <root@sysnode1.psabox.ovh>
2024-09-24 07:43:41 +0000

    NEW FLOWS IMPORT

--- AireBox/scripts/Templates/RestartMachine_script_remote.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM # Author: Ivan Lois (O Grove) - Equipo INDER(PSA Peugeot-Citroen)
-REM # Version: @(#)sftp v.2.8.12.0 // 19-06-14 arce@redcitroen.com
-
-REM # "-----  Reinicio de la maquina de AireBox  -----"
-
-cd %windir%\system32
-WMIC OS Where Primary=TRUE Call Win32Shutdown 6
